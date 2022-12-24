4224dc2543aac608db52e321d9545d8eebad92cc
ihossen016 <ihossen016@gmail.com>
2022-12-24 12:45:11 +0600

    basic file structure and routes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Components
+import Navbar from "./components/Navbar";
+
+// Pages
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App">Hello</div>;
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Navbar />
+
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/cart" element={<Cart />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
