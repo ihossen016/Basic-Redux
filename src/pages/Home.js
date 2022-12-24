f62ef93e610cf87635dc2f9ea0f31d45f5eb44a2
ihossen016 <ihossen016@gmail.com>
2022-12-24 13:35:47 +0600

    products component and dummy products added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Products from "../components/Products";
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <h2 className="heading">Welcome to Redux Store</h2>
+
+            <section>
+                <h3>Products</h3>
+
+                <Products />
+            </section>
         </div>
     );
 };
