a2adc3d7b8ebdf169a34d72a80747953794bef19
ihossen016 <ihossen016@gmail.com>
2022-12-24 18:39:31 +0600

    cart page added and item remove function added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { remove } from "../store/cartSlice";
 
 const Cart = () => {
+    const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart);
+
+    const handleRemove = productId => {
+        dispatch(remove(productId));
+    };
+
     return (
         <div>
             <h2>Cart</h2>
+
+            <div className="cartWrapper">
+                {cartItems.map(item => (
+                    <div className="cartCard">
+                        <img src={item.image} alt="" />
+                        <h4>{item.title}</h4>
+                        <h5>${item.price}</h5>
+                        <button
+                            className="btn"
+                            onClick={() => handleRemove(item.id)}
+                        >
+                            Remove
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
